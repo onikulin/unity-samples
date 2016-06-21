733e91e1d38cc7f10a12de627d50d2707e8841f6
sgoncharov <sgoncharov@cmlatitude.com>
2016-06-21 17:41:20 +0300

    Added workers support for node cellar applications

--- nodecellar-mongodb/src/server.js
+++ nodecellar-mongodb/src/server.js
@@ -1,25 +1,37 @@
 var express = require('express'),
     path = require('path'),
-    wine = require('./routes/wines');
+    wine = require('./routes/wines'),
+    cluster = require('cluster');
 
 var config = require('./config.json');
 var protocol = require(config.server.protocol);
 
-var app = express();
+if (cluster.isMaster && typeof config.server.workers !== 'undefined' && config.server.workers > 0) {
+    var numWorkers = config.server.workers;
+    for (var i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+    cluster.on('online', function (worker) {
+        console.log("Worker " + worker.process.pid + " is online");
+    });
+} else {
+    var app = express();
 
-app.configure(function () {
-    app.set('port', config.server.port);
-    app.use(express.logger('dev'));  /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, 'public')));
-});
+    app.configure(function () {
+        app.set('port', config.server.port);
+        app.use(express.logger('dev'));
+        /* 'default', 'short', 'tiny', 'dev' */
+        app.use(express.bodyParser()),
+            app.use(express.static(path.join(__dirname, 'public')));
+    });
 
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
+    app.get('/wines', wine.findAll);
+    app.get('/wines/:id', wine.findById);
+    app.post('/wines', wine.addWine);
+    app.put('/wines/:id', wine.updateWine);
+    app.delete('/wines/:id', wine.deleteWine);
 
-protocol.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
-});
+    protocol.createServer(app).listen(app.get('port'), function () {
+        console.log("Express server listening on port " + app.get('port'));
+    });
+}
